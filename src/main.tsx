c68cf427020e1057d84e86954ac392fb9318de9d
ChristopherLucasBarbosaAlmeida <lucaschristofher05@gmail.com>
2024-06-08 23:09:47 -0300

    feat: add styles config

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { ScopedCssBaseline } from '@mui/material';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { SnackbarProvider } from 'notistack';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { queryClient } from './libs/queryClient';
-import { router } from './routes/router';
+import { ScopedCssBaseline } from "@mui/material";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { SnackbarProvider } from "notistack";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { queryClient } from "./libs/queryClient";
+import { router } from "./routes/router";
+import "./styles/global.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ScopedCssBaseline>
       <QueryClientProvider client={queryClient}>
@@ -16,5 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </SnackbarProvider>
       </QueryClientProvider>
     </ScopedCssBaseline>
-  </React.StrictMode>,
+  </React.StrictMode>
 );

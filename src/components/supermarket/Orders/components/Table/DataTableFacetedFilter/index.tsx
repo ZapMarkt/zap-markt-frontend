0c6fad3fcaa936454f738738ffdc3b64c24cd190
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-06-12 00:54:36 -0300

    refactor: styling order table

--- src/components/supermarket/Orders/components/Table/DataTableFacetedFilter/index.tsx
+++ src/components/supermarket/Orders/components/Table/DataTableFacetedFilter/index.tsx
@@ -9,34 +9,45 @@ interface DataTableFacetedFilterProps<TData, TValue> {
     value: string;
   }[];
 }
+
 export function DataTableFacetedFilter<TData, TValue>({
   column,
   options,
 }: DataTableFacetedFilterProps<TData, TValue>) {
   const { setFilterValue } = column || {};
 
-  const handleSelect = (value: string) => {
+  const handleSelect = (value?: string) => {
     if (setFilterValue && typeof setFilterValue === 'function') {
       setFilterValue(value === 'Todos' ? undefined : value);
     }
   };
 
   return (
-    <div className="flex flex-wrap items-center space-x-2">
+    <div className="flex flex-wrap items-center w-full mb-6 gap-[18px] border-b max-h-[59px]">
       {options.map((option) => {
         const isSelected = column?.getFilterValue?.() === option.value;
+
         return (
           <Button
             key={option.value}
-            variant={isSelected ? 'default' : 'outline'}
-            size="sm"
             className={cn(
-              'flex items-center space-x-2',
-              isSelected && 'border-primary',
+              'flex items-center px-[18px] pb-5 pt-[10px] border-b-2 border-transparent box-border max-h-[59px]',
+              option.value === 'Todos'
+                ? 'first:border-customMkt-black'
+                : 'first:border-transparent',
+              isSelected && 'border-customMkt-black',
             )}
             onClick={() => handleSelect(option.value)}
           >
-            <span>{option.label}</span>
+            <span
+              className={cn(
+                'text-customMkt-gray6 text-2xl font-medium',
+                option.value === 'Todos' && 'first:text-customMkt-black',
+                isSelected && 'text-customMkt-black',
+              )}
+            >
+              {option.label}
+            </span>
           </Button>
         );
       })}

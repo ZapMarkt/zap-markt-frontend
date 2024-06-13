015a5ffd71132a9e715b8e03f4f27909b5e08545
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-06-13 15:04:11 -0300

    feat: add adress details to order drawer

--- src/components/supermarket/Orders/components/Drawer/components/DrawerAdressDetails/index.tsx
+++ src/components/supermarket/Orders/components/Drawer/components/DrawerAdressDetails/index.tsx
@@ -1,15 +1,15 @@
 const DrawerAdressDetails = () => {
   // TODO need add dinamic data from users
   return (
-    <div className="px-[14px] py-3 flex flex-col gap-1 rounded-[5px] h-[81px] bg-customMkt-whiteF2">
+    <div className="px-[14px] py-3 flex flex-col gap-1 rounded-[5px] bg-customMkt-whiteF2">
       <h1 className="font-semibold text-2xl leading-[29px]">
         Endereço de entrega
       </h1>
-      <div className="flex gap-2 justify-start items-center flex-col">
+      <div className="flex items-start flex-col">
         <p className="font-normal text-xl leading-[24px]">
           Av. Fernandes Lima, 2257 - Pinheiro , Maceió - AL
         </p>
-        <p className="font-normal text-xl leading-[24px]">
+        <p className="font-normal text-xl leading-[24px] text-customMkt-blueLight">
           Ao lado do mercado do joão
         </p>
       </div>

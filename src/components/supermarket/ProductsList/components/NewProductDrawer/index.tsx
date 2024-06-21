46f6923c8d3ca95a223ca611d19e50ca67ae638e
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-06-21 00:43:13 -0300

    style: add paddin to new product drawer

--- src/components/supermarket/ProductsList/components/NewProductDrawer/index.tsx
+++ src/components/supermarket/ProductsList/components/NewProductDrawer/index.tsx
@@ -18,7 +18,7 @@ const NewProductDrawer: React.FC<NewProductDrawerProps> = ({
       <SheetContent className="sm:max-w-[900px] p-0 gap-0 w-full h-full flex flex-col">
         <NewProductDrawerHeader onClose={closeDrawer} />
         <ScrollArea>
-          <div className="flex flex-col gap-6 px-[30px] pt-[30px] pb-5">
+          <div className="flex flex-col gap-6 px-[30px] pt-[30px] pb-[35px]">
             <ProductForm measures={measures} onClose={closeDrawer} />
           </div>
         </ScrollArea>

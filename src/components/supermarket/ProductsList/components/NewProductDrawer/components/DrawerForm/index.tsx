b8ded47f42be214f00f035a4d1705af163046323
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-06-20 21:42:09 -0300

    feat: add form drawer

--- src/components/supermarket/ProductsList/components/NewProductDrawer/components/DrawerForm/index.tsx
+++ src/components/supermarket/ProductsList/components/NewProductDrawer/components/DrawerForm/index.tsx
@@ -15,113 +15,94 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { Switch } from '@/components/ui/switch';
+import { cn } from '@/lib/utils';
+import {
+  NewProductSchema,
+  newProductSchema,
+} from '@/libs/zod/NewProductSchema';
 import { Measure } from '@/types/measure';
 import { zodResolver } from '@hookform/resolvers/zod';
-import React, { useCallback, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useEffect, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { FaBarcode } from 'react-icons/fa';
 import { IoMdCloudUpload } from 'react-icons/io';
-import * as z from 'zod';
 
 interface ProductFormProps {
   measures: Measure[];
 }
 
-const MAX_FILE_SIZE = 3000000;
-const ACCEPTED_IMAGE_TYPES = [
-  'image/jpeg',
-  'image/jpg',
-  'image/png',
-  'image/webp',
-];
-
-const formSchema = z.object({
-  barCode: z
-    .string()
-    .min(8, { message: 'Deve conter pelo menos 1 caractere(s)' })
-    .max(13, { message: 'Deve conter no máximo 13 caractere(s)' }),
-  measureId: z.string().min(1, { message: 'Deve conter pelo menos 1 opção' }),
-  name: z.string().min(1, { message: 'Deve conter pelo menos 1 caractere(s)' }),
-  image: z
-    .any()
-    .refine((file) => file instanceof File, { message: 'Imagem é requerida' })
-    .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
-      message: 'Apenas .jpeg, .jpg, .png e .webp são aceitos',
-    })
-    .refine((file) => file.size <= MAX_FILE_SIZE, {
-      message: 'Tamanho máximo é 3MB.',
-    }),
-  price: z.coerce.number().min(1),
-  isOnPromotion: z.boolean().default(false).optional(),
-  promotionPrice: z.coerce.number().min(1),
-  description: z.string().min(1),
-  categoryId: z.string().min(1),
-  stock: z.string().min(1),
-  pdvCode: z.string().min(1),
-});
-
-type ProductFormValue = z.infer<typeof formSchema>;
-
 const ProductForm: React.FC<ProductFormProps> = ({ measures }) => {
-  const [loading, setLoading] = useState(false);
-  const [file, setFile] = useState<File | null>(null);
-
-  const form = useForm<ProductFormValue>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<NewProductSchema>({
+    resolver: zodResolver(newProductSchema),
     defaultValues: {
       barCode: '',
       measureId: '',
-      image: null,
       name: '',
       price: 0,
-      isOnPromotion: false,
       description: '',
       categoryId: '',
       stock: '',
+      pdvCode: '',
+      isOnPromotion: false,
+      promotionalPrice: 0,
     },
   });
 
-  const onFileChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const selectedFile = event.target.files?.[0] || null;
-      if (
-        (selectedFile && selectedFile.type === 'jpeg') ||
-        (selectedFile && selectedFile.type === 'jpg') ||
-        (selectedFile && selectedFile.type === 'png') ||
-        (selectedFile && selectedFile.type === 'webp')
-      ) {
-      }
-      form.setValue('image', selectedFile);
-      setFile(selectedFile);
-    },
-    [form],
-  );
+  const [loading, setLoading] = useState(false);
 
-  const onSubmit = (data: ProductFormValue) => {
+  // Control if product is on promotion
+  const [promotion, setPromotion] = useState(false);
+
+  useEffect(() => {
+    form.setValue('isOnPromotion', promotion);
+  }, [promotion, form]);
+
+  // Control input image file
+  const [selectedFile, setSelectedFile] = useState<File | undefined>();
+
+  const onSubmit: SubmitHandler<NewProductSchema> = (data) => {
     // TODO: Need add request to create new product
+    if (!promotion) {
+      delete data.promotionalPrice;
+    }
 
     try {
       setLoading(true);
-      alert('Foi seu produto');
       console.log(data);
     } catch (error) {
+      alert('Nao Passou');
       console.log(error);
     } finally {
       setLoading(false);
     }
   };
+
+  // Control of label color in selects
+  const [measureValue, setMeasureValue] = useState('Selecione uma medida');
+  const [categoryValue, setCategoryValue] = useState('Selecione uma categoria');
+  const measureIdValue = form.watch('measureId');
+  const categoryIdValue = form.watch('categoryId');
+
+  useEffect(() => {
+    setMeasureValue(measureIdValue || 'Selecione uma medida');
+    setCategoryValue(categoryIdValue || 'Selecione uma categoria');
+  }, [measureIdValue, categoryIdValue]);
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="grid grid-cols-2 gap-[30px] mb-[22px]">
+        <div className="grid grid-cols-2 gap-[1.875rem] mb-5">
           <FormField
             control={form.control}
             name="barCode"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Código de barras</FormLabel>
+                <FormLabel className="font-medium text-customMkt-gray6">
+                  Código de barras
+                </FormLabel>
                 <FormControl className=" space-y-1">
-                  <div className="flex items-center gap-4 max-h-[56px] p-3 rounded-[5px] border border-customMkt-gray7 focus-within:border-customMkt-primary focus-within:outline-none focus-within:ring-2 focus-within:ring-customMkt-primary transition-all group space-y-reverse space-y-0">
+                  <div className="flex items-center gap-4 max-h-[3.5rem] p-3 rounded-[.3125rem] border border-customMkt-gray7 focus-within:border-customMkt-primary focus-within:outline-none focus-within:ring-2 focus-visible:ring-offset-0 focus-within:ring-customMkt-primary transition-all group space-y-reverse space-y-0">
                     <FaBarcode className="fill-customMkt-gray6 h-6 w-6 transition group-focus-within:fill-customMkt-primary" />
                     <Input
                       disabled={loading}
@@ -148,17 +129,28 @@ const ProductForm: React.FC<ProductFormProps> = ({ measures }) => {
                   defaultValue={field.value}
                 >
                   <FormControl>
-                    <SelectTrigger className="px-3 py-[17px] text-lg font-normal leading-customNormal border-customMkt-gray7 h-[56px] focus-within:border-customMkt-primary focus-within:outline-none focus-within:ring-2 focus-within:ring-customMkt-primary focus:right-0 focus:ring-offset-0 focus:ring-customMkt-primary">
+                    <SelectTrigger
+                      className={cn(
+                        'px-3 py-[1.0625rem] text-lg font-normal leading-customNormal border-customMkt-gray7 h-[3.5rem] focus-within:border-customMkt-primary focus-within:outline-none focus-within:ring-2 focus-within:ring-customMkt-primary focus:right-0 focus:ring-offset-0 focus:ring-customMkt-primary placeholder-select',
+                        measureValue === 'Selecione uma medida' &&
+                          '[&>span]:text-customMkt-gray6',
+                      )}
+                    >
                       <SelectValue
+                        onChange={() => setMeasureValue(field.value)}
                         defaultValue={field.value}
                         placeholder="Selecione uma medida"
                         className="border-none outline-none"
                       />
                     </SelectTrigger>
                   </FormControl>
-                  <SelectContent>
+                  <SelectContent className="p-0">
                     {measures.map((measure) => (
-                      <SelectItem key={measure.type} value={measure.type}>
+                      <SelectItem
+                        key={measure.type}
+                        value={measure.type}
+                        className="border-b border-customMkt-whiteF0 text-lg font-normal last:border-b-0 rounded-b-0"
+                      >
                         {measure.name}
                       </SelectItem>
                     ))}
@@ -172,38 +164,227 @@ const ProductForm: React.FC<ProductFormProps> = ({ measures }) => {
         <FormField
           control={form.control}
           name="image"
-          render={() => (
-            <FormItem>
-              <div className="w-full h-[440px] outline-dashed outline-1 outline-customMkt-gray7 rounded-[5px] relative">
-                {file ? (
+          render={({ field }) => (
+            <FormItem className="mb-5">
+              <div className="w-full h-[27.5rem] outline-dashed outline-1 outline-customMkt-gray7 rounded-[.3125rem] relative">
+                {selectedFile ? (
                   <div>
                     <img
-                      src={URL.createObjectURL(file)}
+                      src={URL.createObjectURL(selectedFile)}
                       alt="Imagem do Produto"
-                      className="absolute justify-center items-center h-full w-full gap-3 rounded-[5px]"
+                      className="absolute justify-center items-center h-full w-full gap-3 rounded-[.3125rem] object-contain"
                     />
                   </div>
                 ) : (
-                  <div className="absolute justify-center items-center h-full w-full gap-3 rounded-[5px]">
-                    <div className="p-[10px] rounded-[8px] border border-customMkt-gray1 bg-white text-center group-hover:border-customMkt-primary transition">
-                      <IoMdCloudUpload className="w-[18px] h-[18px] fill-customMkt-gray6 group-hover:fill-customMkt-primary" />
+                  <div className="absolute flex justify-center items-center h-full w-full gap-3 rounded-[.3125rem]">
+                    <div className="p-[.625rem] rounded-[.5rem] border border-customMkt-gray1 bg-white text-center group-hover:border-customMkt-primary transition h-[2.375rem] w-[2.375rem]">
+                      <IoMdCloudUpload className="w-[1.125rem] h-[1.125rem] fill-customMkt-gray6 group-hover:fill-customMkt-primary" />
+                    </div>
+                    <div className="flex items-center justify-center flex-col">
+                      <p className=" text-sm font-semibold text-customMkt-primary leading-[1.25rem]">
+                        Arraste uma foto ou clique para subir
+                      </p>
+                      <span className="text-xs font-normal leading-6">
+                        Arquivos aceitos PNG, JPG, e WEBP (3mb máx)
+                      </span>
                     </div>
                   </div>
                 )}
-
-                <Input
+                <input
+                  {...form.register('image')}
                   type="file"
+                  accept=".png, .jpeg, .jpg, .webp"
                   disabled={loading}
-                  placeholder="Pesquisar código de barras"
-                  onChange={onFileChange}
-                  className="w-full h-full opacity-0"
+                  onChange={(event) => {
+                    field.onChange(event.target.files);
+                    setSelectedFile(event.target.files?.[0]);
+                  }}
+                  className="w-full h-full opacity-0 "
                 />
               </div>
-
               <FormMessage />
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="mb-5">
+              <FormLabel>Nome do produto</FormLabel>
+              <FormControl>
+                <Input
+                  className="px-3 py-[1.0625rem] text-lg font-normal leading-customNormal border-customMkt-gray7 h-[3.5rem]"
+                  disabled={loading}
+                  placeholder="Insira o nome do produto"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="grid grid-cols-2 gap-[1.875rem] mb-5">
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Preço</FormLabel>
+                <FormControl className=" space-y-1">
+                  <div className="flex items-center gap-4 max-h-[3.5rem] p-3 rounded-[.3125rem] border border-customMkt-gray7 focus-within:border-customMkt-primary focus-within:outline-none focus-within:ring-2 focus-within:ring-customMkt-primary transition-all group space-y-0 mt-0">
+                    <p className="text-lg font-normal fill-customMkt-gray6 h-6 w-6 transition group-focus-within:text-customMkt-primary group-focus-within:stroke-customMkt-primary">
+                      R$
+                    </p>
+                    <Input
+                      type="number"
+                      disabled={loading}
+                      placeholder="0,00"
+                      {...field}
+                      className="p-0 text-lg font-normal leading-5 placeholder:text-customMkt-gray6 focus-visible:ring-transparent focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:ring-offset-inherit border-0 "
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="promotionalPrice"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Preço promocional</FormLabel>
+                <FormControl className=" space-y-1">
+                  <div className="flex items-center gap-4 max-h-[3.5rem] p-3 rounded-[.3125rem] border border-customMkt-gray7 focus-within:border-customMkt-primary focus-within:outline-none focus-within:ring-2 focus-within:ring-customMkt-primary transition-all group space-y-0 mb-0">
+                    <p className="text-lg font-normal fill-customMkt-gray6 h-6 w-6 transition group-focus-within:text-customMkt-primary group-focus-within:stroke-customMkt-primary">
+                      R$
+                    </p>
+                    <Input
+                      type="number"
+                      disabled={loading || !promotion}
+                      placeholder="0,00"
+                      {...field}
+                      className="p-0 text-lg font-normal leading-5 placeholder:text-customMkt-gray6 focus-visible:ring-transparent focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:ring-offset-inherit border-0"
+                      style={{ marginBottom: '0px !important' }}
+                    />
+                    <Switch
+                      className=" data-[state=checked]:bg-customMkt-primary data-[state=unchecked]:bg-input"
+                      onCheckedChange={() => setPromotion((prev) => !prev)}
+                      {...form.register('isOnPromotion')}
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem className="mb-5">
+              <FormLabel>Descrição</FormLabel>
+              <FormControl>
+                <Input
+                  className="px-3 py-[1.0625rem] text-lg font-normal leading-customNormal border-customMkt-gray7 h-[3.5rem]"
+                  disabled={loading}
+                  placeholder="Insira a descrição do produto"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="grid grid-cols-2 gap-[1.875rem] mb-5">
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Categoria</FormLabel>
+                <Select
+                  disabled={loading}
+                  onValueChange={field.onChange}
+                  value={field.value}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger
+                      className={cn(
+                        'px-3 py-[1.0625rem] text-lg font-normal leading-customNormal border-customMkt-gray7 h-[3.5rem] focus-within:border-customMkt-primary focus-within:outline-none focus-within:ring-2 focus-within:ring-customMkt-primary focus:right-0 focus:ring-offset-0 focus:ring-customMkt-primary placeholder-select',
+                        categoryValue === 'Selecione uma categoria' &&
+                          '[&>span]:text-customMkt-gray6',
+                      )}
+                    >
+                      <SelectValue
+                        onChange={() => setCategoryValue(field.value)}
+                        defaultValue={field.value}
+                        placeholder="Selecione uma categoria"
+                        className="border-none outline-none"
+                      />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="p-0">
+                    {measures.map((measure) => (
+                      <SelectItem
+                        key={measure.type}
+                        value={measure.type}
+                        className="border-b border-customMkt-whiteF0 text-lg font-normal last:border-b-0 rounded-b-0"
+                      >
+                        {measure.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-[1.875rem] mb-5">
+          <FormField
+            control={form.control}
+            name="stock"
+            render={({ field }) => (
+              <FormItem className="mb-5">
+                <FormLabel>Estoque</FormLabel>
+                <FormControl>
+                  <Input
+                    className="px-3 py-[1.0625rem] text-lg font-normal leading-customNormal border-customMkt-gray7 h-[3.5rem]"
+                    disabled={loading}
+                    placeholder="Insira o estoque do seu produto"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="pdvCode"
+            render={({ field }) => (
+              <FormItem className="mb-5">
+                <FormLabel>Código PDV</FormLabel>
+                <FormControl>
+                  <Input
+                    className="px-3 py-[1.0625rem] text-lg font-normal leading-customNormal border-customMkt-gray7 h-[3.5rem]"
+                    disabled={loading}
+                    placeholder="Insira o codigo PDV do seu produto"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
         <Button disabled={loading} className="ml-auto" type="submit">
           Salvar
         </Button>
--- src/components/supermarket/ProductsList/components/NewProductDrawer/index.tsx
+++ src/components/supermarket/ProductsList/components/NewProductDrawer/index.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { Sheet, SheetContent } from '@/components/ui/sheet';
 import { measures } from '@/data/measure-data';
 import ProductForm from './components/DrawerForm';
@@ -14,11 +15,13 @@ const NewProductDrawer: React.FC<NewProductDrawerProps> = ({
 }) => {
   return (
     <Sheet open={openDrawer}>
-      <SheetContent className="sm:max-w-[900px] p-0 gap-0 w-full flex flex-col">
+      <SheetContent className="sm:max-w-[900px] p-0 gap-0 w-full h-full flex flex-col">
         <NewProductDrawerHeader onClose={closeDrawer} />
-        <div className="p-[30px] pb-[35px] w-full h-full">
-          <ProductForm measures={measures} />
-        </div>
+        <ScrollArea>
+          <div className="flex flex-col gap-6 px-[30px] pt-[30px] pb-5">
+            <ProductForm measures={measures} />
+          </div>
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );

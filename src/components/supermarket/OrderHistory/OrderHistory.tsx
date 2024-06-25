40b7c38e2243fe189f25ddba195c642e3cc51326
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-06-24 23:37:00 -0300

    style: add header with data pick and button filter

--- src/components/supermarket/OrderHistory/OrderHistory.tsx
+++ src/components/supermarket/OrderHistory/OrderHistory.tsx
@@ -1,12 +1,22 @@
 import SearchInput from '@/components/ui/SearchInput';
+import { Button } from '@/components/ui/button';
 import { DatePickerWithRange } from '@/components/ui/data-picker-ranger';
+import { FaFilter } from 'react-icons/fa';
 
 const OrderHistoryTable = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between mb-[30px]">
       <SearchInput placeholder="Pesquisar por pedidos" />
-      <div>
+      <div className="flex gap-4">
         <DatePickerWithRange />
+        <Button
+          size="customMd"
+          variant="customOutiline"
+          className="gap-[10px] transition max-w-[109px] w-full"
+        >
+          <FaFilter className="fill-customMkt-gray6 group-hover:fill-customMkt-primary/80 w-6 h-6" />
+          Filtrar
+        </Button>
       </div>
     </div>
   );
--- src/components/ui/data-picker-ranger.tsx
+++ src/components/ui/data-picker-ranger.tsx
@@ -1,20 +1,15 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { addDays, format } from 'date-fns';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { useForm } from 'react-hook-form';
-import { IoCalendar } from 'react-icons/io5';
+import { IoCalendar, IoSearch } from 'react-icons/io5';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form';
+import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import {
   Popover,
   PopoverContent,
@@ -25,98 +20,94 @@ import { useState } from 'react';
 import { DateRange } from 'react-day-picker';
 
 const FormSchema = z.object({
-  dob: z
-    .object({
-      from: z
-        .date({
-          required_error: 'Start date is required.',
-        })
-        .nullable(),
-      to: z
-        .date({
-          required_error: 'End date is required.',
-        })
-        .nullable()
-        .optional(),
-    })
-    .refine((data) => data.from && data.to && data.from <= data.to, {
-      message: 'End date cannot be before start date',
-      path: ['to'],
-    }),
+  dateForm: z.object(
+    {
+      from: z.date(),
+      to: z.date().optional(),
+    },
+    { message: 'Insira uma data válida' },
+  ),
 });
 
 export function DatePickerWithRange({}: React.HTMLAttributes<HTMLDivElement>) {
-  const [date, setDate] = useState<DateRange>({
-    from: new Date(),
-    to: addDays(new Date(), 10),
-  });
-
+  const [date, setDateRange] = useState<DateRange | undefined>();
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log(data);
+    setDateRange(data.dateForm);
+    setIsPopoverOpen(false);
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="dob"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={'outline'}
-                      className={cn(
-                        'w-[258px] max-h-[58px] px-3 py-[17px] text-left font-normal border border-customMkt-gray7 rounded-[5px] flex justify-center items-center gap-[10px]',
-                        !(field.value?.from && field.value?.to) &&
-                          'text-muted-foreground',
-                      )}
-                    >
-                      {field.value?.from && field.value?.to ? (
-                        `${format(field.value.from, 'PPP')} - ${format(
-                          field.value.to,
-                          'PPP',
-                        )}`
-                      ) : (
-                        <span className="text-lg text-customMkt-gray6 font-medium leading-customNormal">
-                          Data personalizada
-                        </span>
-                      )}
-                      <IoCalendar className="ml-auto h-6 w-6 fill-customMkt-gray6" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
+    <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-[258px] max-h-[58px] px-3 py-[17px] text-left font-normal border border-customMkt-gray7 rounded-[5px] flex justify-center items-center gap-[10px] group hover:text-customMkt-primary/80 hover:border-customMkt-primary/80 hover:fill-customMkt-primary',
+            !date?.from && !date?.to && 'text-muted-foreground',
+          )}
+        >
+          {date?.from ? (
+            date?.to ? (
+              <>
+                {format(date.from, 'dd/MM/yyyy', { locale: ptBR })} -{' '}
+                {format(date.to, 'dd/MM/yyyy', { locale: ptBR })}
+              </>
+            ) : (
+              format(date.from, 'dd/MM/yyyy', { locale: ptBR })
+            )
+          ) : (
+            <span className="text-lg text-customMkt-gray6 font-medium leading-customNormal group-hover:text-customMkt-primary">
+              Data personalizada
+            </span>
+          )}
+          <IoCalendar
+            className={cn(
+              'ml-auto h-6 w-6 group-hover:fill-customMkt-primary',
+              isPopoverOpen ? 'fill-customMkt-primary' : 'fill-customMkt-gray6',
+            )}
+          />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="dateForm"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
                   <Calendar
+                    className={{
+                      day_range_middle: 'aria-selected:bg-customMkt-secondary',
+                    }}
                     initialFocus
                     mode="range"
                     defaultMonth={date?.from}
-                    selected={field.value as DateRange}
+                    selected={field.value}
                     onSelect={(range) => {
-                      setDate(range as DateRange);
-                      field.onChange({
-                        from: range?.from || undefined,
-                        to: range?.to || null,
-                      });
+                      field.onChange(range);
+                      setDateRange(range);
                     }}
                     disabled={(date) =>
                       date > new Date() || date < new Date('1900-01-01')
                     }
                   />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button size="customLg" variant="customPrimary" type="submit">
+              <IoSearch className="mr-1" /> Buscar
+            </Button>
+          </form>
+        </Form>
+      </PopoverContent>
+    </Popover>
   );
 }
